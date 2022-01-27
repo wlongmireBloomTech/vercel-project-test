5fad9bc2c9d25a13084bd353afff5b017ba30fae
Warren C. Longmire <warren.longmire@lambdaschool.com>
2022-01-27 13:19:32 -0500

    added git add .

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>Warren's Big Money Making Machine</h1>
-      <p>This is a subheading.</p>
+      <p>This is a subheading!!!!!</p>
       <button>Deposit $1</button>
     </div>
   );
